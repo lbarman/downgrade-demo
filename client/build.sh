84483daf845382896b6a70ceb3772fe0d658e562
Ludovic Barman <lbarman@protonmail.ch>
2017-06-06 00:04:24 +0200

    Adds client that connects to lbarman.ch

---
+++ client/build.sh
@@ -0,0 +1,4 @@
+#sudo docker build -t lbarman/downgrade-demo-client:latest . 
+#sudo docker push lbarman/downgrade-demo-client:latest &
+
+sudo docker build --no-cache=true -t downgrade-demo-client .
